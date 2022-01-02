e0dac4adaaef8243558b50c986666e18e87fa11f
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2022-01-02 16:15:02 +0530

    Update editfile.js

--- editfile.js
+++ editfile.js
@@ -4,20 +4,26 @@ const file = 'README.md';
 const options = {
     hostname: 'picsum.photos',
     path: '/200/300',
+    port: 80,
     method: 'GET'
 }
 
-const req = https.request(options, res => {
-    res.on('data', d => {
+const req = http.request(options, function (res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
 
+    res.on('data', function (chunk) {
+        const data = chunk;
         fs.readFile(file, function (err, data) {
             if (err) throw err;
-            fs.writeFile(file, d, function (err) {
+            fs.writeFile(file, data, function (err) {
                 err || console.log('Data replaced \n', data);
             });
         });
-    })
-})
+    });
+});
+
 
 req.on('error', error => {
     console.error(error)
