f08b150cb8c393847dca3f5d0439e138820238f6
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2022-01-02 16:44:39 +0530

    Update editfile.js

--- editfile.js
+++ editfile.js
@@ -5,7 +5,7 @@ var replace = require("replace");
 let r = (Math.random() + 1).toString(36).substring(7);
 replace({
     regex: "<!--aa-->",
-    replacement: `![](https://picsum.photos/25)<!--aa-->`,
+    replacement: `![](https://picsum.photos/25?${r}=tr)<!--aa-->`,
     paths: ['README.md'],
     recursive: true,
     silent: true,
