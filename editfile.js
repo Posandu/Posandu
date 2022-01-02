603b68a3a8d69b278f67fea151087868721fad63
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2022-01-02 16:06:42 +0530

    Update editfile.js

--- editfile.js
+++ editfile.js
@@ -5,7 +5,7 @@ fs.readFile(someFile, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace("<!----!----->", '<!----!-----> <img src="https://picsum.photos/200/300">');
+  var result = data.replace(/<!----!----->/g, '<!----!-----> <img src="https://picsum.photos/200/300">');
 
   fs.writeFile(someFile, result, 'utf8', function (err) {
      if (err) return console.log(err);
