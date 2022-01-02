8857eb57a6495bdb85a98b312dc54289e0e2350d
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2022-01-02 16:26:12 +0530

    Update editfile.js

--- editfile.js
+++ editfile.js
@@ -1,23 +1,8 @@
-const Jimp = require('jimp');
-
-
-let imageData = [
-  [ 0xFF0000FF, 0xFF0000FF, 0xFF0000FF ],
-  [ 0xFF0000FF, 0x00FF00FF, 0xFF0000FF ],
-  [ 0xFF0000FF, 0xFF0000FF, 0x0000FFFF ]
-];
-
-
-let image = new Jimp(3, 3, function (err, image) {
-  if (err) throw err;
-
-  imageData.forEach((row, y) => {
-    row.forEach((color, x) => {
-      image.setPixelColor(color, x, y);
-    });
-  });
-
-  image.write('img.png', (err) => {
-    if (err) throw err;
-  });
+var fs = require('fs');
+var imgGen = require('js-image-generator');
+ 
+// Generate one image
+imgGen.generateImage(800, 600, 80, function(err, image) {
+    fs.writeFileSync('img.jpg', image.data);
 });
+ 
