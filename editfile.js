3a36f1bde41ef181331b3c0bf4b4b087345c6c12
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2022-01-02 16:33:44 +0530

    Update editfile.js

--- editfile.js
+++ editfile.js
@@ -5,7 +5,7 @@ var replace = require("replace");
 let r = (Math.random() + 1).toString(36).substring(7);
 replace({
     regex: "<!--START_SECTION:waka-->",
-    replacement: "<img src='https://robohash.org/"+r+"'> <!--START_SECTION:waka-->",
+    replacement: "<img src='https://robohash.org/"+r+".png' width='200'> <!--START_SECTION:waka-->",
     paths: ['README.md'],
     recursive: true,
     silent: true,
