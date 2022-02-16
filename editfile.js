61e076e02d14102d3c94ed2998414828f53ec3e2
Posandu Mapa <posandumapa@gmail.com>
2022-02-16 19:23:38 +0530

    o ye

--- editfile.js
+++ editfile.js
@@ -1,11 +1,9 @@
-var replace = require("replace");
-
-// use:
-
+const replace = require("replace");
 let r = (Math.random() + 1).toString(36).substring(7);
+const reg = "/****\\**\\**/";
 replace({
-    regex: "//",
-    replacement: `\nconst _${(Math.random() + 1).toString(36).substring(7)} = \`${r}__${new Date().toLocaleString()}\`;//`,
+    regex: reg,
+    replacement: `\nconst _${(Math.random() + 1).toString(36).substring(7)} = \`${r}__${new Date().toLocaleString()}\`; `+reg,
     paths: ['README.md'],
     recursive: true,
     silent: true,
