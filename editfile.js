fdeeea452f905c0acdc066e4eef48028485b6a99
Posandu Mapa <posandumapa@gmail.com>
2022-02-17 16:21:47 +0530

    gh

--- editfile.js
+++ editfile.js
@@ -3,7 +3,7 @@ let r = (Math.random() + 1).toString(36).substring(7);
 const reg = "a=10;";
 replace({
     regex: reg,
-    replacement: `\nconst _${(Math.random() + 1).toString(36).substring(7)} = \`${r}__${new Date().toLocaleString()}\`; `+reg,
+    replacement: `\`${r}__${new Date().toLocaleString()}\`;`+reg,
     paths: ['README.md'],
     recursive: true,
     silent: true,
